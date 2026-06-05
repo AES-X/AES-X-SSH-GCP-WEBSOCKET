6fb0f8f66360aaa9b098bc72ebc7df17a4d786a6
AES X <163116712+AES-X@users.noreply.github.com>
2026-06-05 13:44:19 +0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 --max-connections-for-client 20 &
-dropbear -R -E -F -p 127.0.0.1:40000 -a -W 65535 &
+dropbear -R -E -F -p 127.0.0.1:40000 -a -W 65535 -b /etc/dropbear_banner &
 sleep 1
 ./proxy
-
